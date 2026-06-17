dddf4790b20c3baf8cb81c0dd800fdc4f242228d
Marcos Felipe <132904349+felipeillaoi@users.noreply.github.com>
2026-06-17 13:12:57 -0300

    V 1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,21 @@ function toggleTheme() {
   // O 'toggle' adiciona a classe se ela não existir, e remove se ela já existir.
   document.body.classList.toggle('dark-mode');
 }
+
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+}
+
+// 2. FUNÇÃO DO BOTÃO DE ALTERNAR TEMA
+function toggleTheme() {
+    // Adiciona ou remove a classe
+    document.body.classList.toggle('dark-mode');
+    
+    // Salva a escolha na memória do navegador
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+}
